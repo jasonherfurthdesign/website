8e05111ba7d23406c94d31636e2a1e0e70569f57
Jay <70462126+grayf3ld@users.noreply.github.com>
2024-07-26 14:03:56 -0700

    hope this works

--- jhd-website/src/App.js
+++ jhd-website/src/App.js
@@ -64,7 +64,6 @@ const GraphicDesignPortfolio = () => {
 
   return (
     <div className={`min-h-screen font-sans ${darkMode ? 'dark' : ''}`}>
-      {/* Skip to main content link */}
       <a href="#main-content" className="sr-only focus:not-sr-only focus:absolute focus:top-0 focus:left-0 bg-blue-500 text-white p-2">
         Skip to main content
       </a>
